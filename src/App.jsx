14e2b277ab505c5dfb2939fce2fd99531d08c49f
Antônio Elvis <githubantonioelvis@gmail.com>
2025-01-02 11:52:37 -0300

    feat: adicionar estrutura inicial da sidebar e ajustes no projeto
    - Criar componentes `Header` e `Sidebar` com seus respectivos arquivos CSS
    - Adicionar páginas `Post` e `Post.module.css`
    - Reorganizar imagens em `src/assets/images` (renomear e adicionar novas)
    - Estilizar globalmente com `global.css` e remover `index.css`
    - Atualizar arquivos principais: `App.jsx`, `App.module.css`, `main.jsx`, `index.html`
    - Ajustar dependências em `package.json` e `package-lock.json`

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { Header } from "./components/Header"
+
+import './global.css'
+import style from './App.module.css'
+
+import { Post } from "./pages/Post"
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
+      <div className={style.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post author="Elvis" content="
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis repellat eos consequuntur quos blanditiis iusto 
+    " />
+          <Post author="Elvis" content="
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis repellat eos consequuntur quos blanditiis iusto 
+    " />
+        </main>
+      </div>
     </>
   )
 }
-
 
