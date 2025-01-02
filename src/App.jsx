0be8ac4ceefa9e1e5e774af77334cc0b2e7b15ec
Antônio Elvis <githubantonioelvis@gmail.com>
2025-01-02 12:31:57 -0300

    refactor: mover componente Post para a pasta de componentes
    - Criar `Post.jsx` e `Post.module.css` na pasta `components`
    - Remover arquivos `Post.jsx` e `Post.module.css` da pasta `pages`
    - Ajustar importações no `App.jsx` para refletir a nova estrutura

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Header } from "./components/Header"
 import './global.css'
 import style from './App.module.css'
 
-import { Post } from "./pages/Post"
 import { Sidebar } from './components/Sidebar';
 
+import { Post } from "./components/Post";
 export function App() {
 
   return (
